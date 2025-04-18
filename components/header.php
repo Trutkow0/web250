af537939eed78dbdc35ede73e8f64c8d24b19066
Timothy Rutkowski <tim.rutkowski@gmail.com>
2025-04-18 20:24:47 +0000

    Update WEB250

--- components/header.php
+++ components/header.php
@@ -1,8 +1,16 @@
 <header>
 	<div class="banner">
-		<img id="logo" src="images/TR_logo250.png" alt="Tenacious Raccoon logo">
-                <h1 id="banner-text">Timothy Rutkowski's Tenacious Raccoon | WEB250 </h1>
-        </div>
+		<div class="banner-left">
+			<img id="logo" src="images/TR_logo250.png" alt="Tenacious Raccoon logo">
+                	<h1 id="banner-text">Timothy Rutkowski's Tenacious Raccoon | WEB250 </h1>
+		</div>
+
+		<div class="multipage-links">
+			Multipage Sites:
+			<a href="http://50.116.33.156/web250/multipage_sites/superduper_static" target="_blank">Static</a> |
+			<a href="http://50.116.33.156/web250/multipage_sites/superduper_php" target="_blank">PHP</a>
+		</div>
+	</div>
 </header>
 
 
